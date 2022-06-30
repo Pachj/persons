22b8dd58723e18dcf8986a0dbf7a29e708bd1bbe
Henry Joerg <henry.joerg@gmail.com>
2022-06-30 10:13:58 +0200

    controller finished

--- src/main/java/ch/bbw/persons/MainController.java
+++ src/main/java/ch/bbw/persons/MainController.java
@@ -4,8 +4,10 @@ import ch.bbw.persons.model.Person;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -29,15 +31,32 @@ public class MainController {
         return "persons";
     }
 
-    @GetMapping("/addPerson")
+    @GetMapping("/editPerson")
     public String addPerson(Model model) {
         model.addAttribute("person", new Person());
-        return "addPerson";
+        return "editPerson";
     }
 
-    @PostMapping("/addPerson")
-    public String addNewPersonOrChangePerson(@ModelAttribute @Valid Person person) {
+    @PostMapping("/editPerson")
+    public String addNewPersonOrChangePerson(@ModelAttribute @Valid Person person, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "editPerson";
+        }
+
         personService.savePerson(person);
+        return "redirect:/persons";
+    }
+
+    @GetMapping("/editPerson/{id}")
+    public String editPerson(Model model, @PathVariable int id) {
+        System.out.println(personService.getPersonById(id));
+        model.addAttribute("person", personService.getPersonById(id));
+        return "editPerson";
+    }
+
+    @PostMapping("/editPerson/{id}/delete")
+    public String deletePerson(@PathVariable int id) {
+        personService.deletePerson(id);
         return "redirect:/persons";
     }
 }
